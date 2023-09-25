d3c63a1c2ff33a88639de989500331a13267f7e3
Abhishek5723 <104137737+Abhishek5723@users.noreply.github.com>
2023-09-25 11:00:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('Hello jee');
+
+let lastname='babbar';
+let firstname=new String('love');
+
+let message='this is my first message';
+let copy=message.split(' ');
+console.log(copy);
+
+let message2 =`this
+is my new message using backtik`;
+console.log(message2);
+
+let tarik=new Date();
+console.log(tarik);
+
+
+
+let tarik2=new Date(1998,11,20,7)
+console.log(tarik2);
+
+tarik2.setFullYear(2002);
+tarik2.getFullYear();
+console.log(tarik2);
